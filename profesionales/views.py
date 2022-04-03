c1029be457de1a23f6f1fdbe2e46b9122ed8ba7c
Antonella M Giusiano <antogiusiano31@gmail.com>
2022-04-03 15:47:28 -0300

    Login

--- profesionales/views.py
+++ profesionales/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import redirect, render
 from .models import Cerrajero
 from .forms import CerrajeroFormulario, CerrajeroBusqueda
+from django.views.generic.detail import DetailView
+from django.views.generic.edit import UpdateView, DeleteView
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
 
-# Create your views here.
-
+@login_required
 def crear_cerrajero(request):
     
     if request.method == 'POST':
@@ -31,3 +34,18 @@ def lista_cerrajeros(request):
     
     form = CerrajeroBusqueda()
     return render(request, 'profesionales/lista_cerrajeros.html',{'form':form, 'cerrajeros': cerrajeros})
+
+
+class DetalleCerrajero(DetailView):
+    model = Cerrajero
+    template_name = 'profesionales/detalle_cerrajero.html'
+
+# Igual se hace con el create
+class EditarCerrajero(LoginRequiredMixin, UpdateView):
+    model = Cerrajero
+    success_url = '/profesionales/cerrajeros/'
+    fields = ['nombre', 'apellido', 'desempleado']
+
+class BorrarCerrajero(LoginRequiredMixin, DeleteView):
+    model = Cerrajero
+    success_url = '/profesionales/cerrajeros/'
